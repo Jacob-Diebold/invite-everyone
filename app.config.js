4925aa1f855c8a7f2254053be2729a999fcd97bf
Jacob Diebold <diebold.jacob@gmail.com>
2024-03-31 13:51:35 -0600

    chore: move eas id to env

--- app.config.js
+++ app.config.js
@@ -1,10 +1,16 @@
 const bugsnagKey = process.env.EXPO_PUBLIC_BUGSNAG_KEY;
+const easProjectId = process.env.EXPO_PUBLIC_EAS_PROJECT_ID;
 export default ({ config }) => ({
   ...config,
   extra: {
     ...config.extra,
     bugsnag: {
+      ...config.extra.bugsnag,
       apiKey: bugsnagKey,
+    },
+    eas: {
+      ...config.extra.eas,
+      projectId: easProjectId,
     },
   },
 });
